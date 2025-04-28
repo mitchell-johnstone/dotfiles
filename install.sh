dfd1492153c36c71f5f28c6c64f51271c2ede2e0
Mitchell Johnstone <mitchell.johnstone@directsupply.com>
2025-04-28 10:30:03 -0500

    remove snap, install nvim from source

--- install.sh
+++ install.sh
@@ -7,23 +7,24 @@ sudo apt upgrade
 # Download needed packages
 sudo apt install stow # Download stow
 sudo apt install snapd # install snapd to install packages
-sudo snap install core # install snapd runtime env
-sudo snap install curl # install curl command, used later
+sudo apt install curl # install curl!
+sudo apt install cmake  # version 3.22.1-1ubuntu1.22.04.2
 
 # For cowsay / lolcat / fortune fun
 sudo apt-get install cowsay fortune lolcat # install cowsay features
 
 # Languages!
 sudo apt install build-essential
-sudo snap install clangd --classic # install c++ content
-sudo snap install node --classic # install nodejs content
-sudo snap install pyright --classic # install python content
+sudo apt-get install clangd-12 # c++ compiler
+sudo apt install nodejs # js content
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # rust!
 
-# Add difftastic!
-sudo snap install difftastic
+# Add other tools!
+sudo apt install bat
+cargo install --locked difftastic
 
 # }}}
-# VIM / NVIM {{{
+# VIM / NVIM / TMUX {{{
 # Remove old files
 rm -rf ~/.local/share/nvim
 rm -rf ~/.config/nvim
@@ -31,23 +32,23 @@ rm -rf ~/.vim
 rm -rf ~/.tmux.conf
 rm -rf ~/.tmux
 rm -rf ~/.bash_aliases
+rm -rf ~/.bashrc
 
 # Tools for VIM/NVIM
 sudo apt-get install ripgrep # BurntSushi/ripgrep github
 sudo apt install universal-ctags # install Universal ctags for gutentags
 
-# Install neovim
-# nightly builds for latest, from https://github.com/neovim/neovim/blob/master/INSTALL.md
-sudo snap install --edge nvim --classic
+# Install neovim (build from source)
+# follow comment from here https://www.reddit.com/r/neovim/comments/146dssj/issue_with_getting_the_latest_neovim_on_my_ubuntu/
+git clone https://github.com/neovim/neovim.git
+pushd neovim
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+popd
+rm -rf neovim
 
 # Install vim
 sudo apt install vim
-
-# }}}
-# TMUX {{{
-
-# tmuxinator
-gem install tmuxinator
 
 # Uses TPM for package manager
 # https://github.com/tmux-plugins/tpm
@@ -55,12 +56,6 @@ gem install tmuxinator
 
 # }}}
 # Bash Setup {{{
-
-# Only add grep
-if [[ -z $(grep -F "fortune | cowsay | lolcat" ~/.bashrc) ]]
-then
-    echo "fortune | cowsay | lolcat" >> ~/.bashrc
-fi
 
 # Add in the Tao of the Programming to fortune teller
 sudo sh -c 'cat fortune/tao-compressed | base64 -d | gunzip > /usr/share/games/fortunes/tao'
